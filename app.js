41dd67dbc2e6cdd990cc31168eb7b344fc84a163
sanmijingbrahma <bsanmijing@gmail.com>
2025-07-09 14:08:52 +0530

    Build basic CRUD Structure

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express();
+
+const PORT = 8000;
+
+app.use(express.json())
+
+
+// Routes
+
+app.get("/articles",(req,res)=>{
+	res.send("Here are the Articles");
+})
+
+
+app.get("/articles/:id",(req,res)=>{
+	res.send("Here is the articles by this id");
+})
+
+app.post("/articles",(req,res)=>{
+	let name = prompt("What is your name");
+})
+
+app.delete("/articles/:id",(req,res)=>{
+	res.send("article of this id is deleted")
+})
+
+app.put("/articles/:id",(req,res)=>{
+	res.send("article of this id is updated.")
+})
+
+
+app.listen(PORT,()=>{
+	console.log("I am Listening.....");
+})
+
